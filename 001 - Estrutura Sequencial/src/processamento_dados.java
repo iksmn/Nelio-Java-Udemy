2b93a476f59ea0dfc999f15ae48fa670c79aeacb
Icaro <iksmn@proton.me>
2025-04-03 17:05:58 -0300

    Processamento de Dados

---
+++ 001 - Estrutura Sequencial/src/processamento_dados.java
@@ -0,0 +1,50 @@
+public class processamento_dados {
+    public static void main (String[] args){
+
+        int x,y;
+
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        int a;
+        double c;
+
+        a = 3;
+        c = 2 * a;
+
+        System.out.println(a);
+        System.out.println(c);
+
+
+        double b, B, h, area;
+
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+
+        area = (b + B) / 2.0 * h;
+
+        System.out.println(area);
+
+        int m, n;
+        double resultado;
+
+        m = 5;
+        n = 2;
+
+        resultado = (double) m/n; //casting - conversão do tipo de variável
+
+        System.out.println(resultado);
+
+        double j;
+        int k;
+
+        j = 5.0;
+        k = (int) j;
+
+        System.out.println(k);
+    }
+}
