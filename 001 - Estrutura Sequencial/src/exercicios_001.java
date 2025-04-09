eb5bcfe354fa44d021806222ac45617f459d427c
Icaro <iksmn@proton.me>
2025-04-09 17:34:17 -0300

    Estrutura Sequencial

---
+++ 001 - Estrutura Sequencial/src/exercicios_001.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class exercicios_001 {
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner (System.in);
+
+        int a, b, soma;
+        System.out.println("Digite o primeiro número: ");
+        a = sc.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        b = sc.nextInt();
+
+        soma = a + b;
+
+        System.out.println("A soma dos números é: " + soma);
+
+        sc.close();
+    }
+}
